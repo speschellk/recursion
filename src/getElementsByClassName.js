3f2b5661f16817c6c726b60ade3ef24ff0bf782e
Kay Christensen <kaychristensen@gmail.com>
2016-11-27 21:34:46 -0800

    created function skeleton

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var elements = document.querySelectorAll(".targetClassName");
+
+  //recursive function that iterates over each text node in the dom
+      //termination condition
+      if (className = undefined) {
+        return "Please enter a class name to search for.";
+
+      // base case
+      } else if  {
+        // done searching through document, stop
+
+      //recursive case
+      } else {
+        getElementsByClassName('something smaller')
+      };
+
+  return elements;
 };
