8858b56c2540f47c82e57cf2d14e03a85e4b0cc9
Kay Christensen <kaychristensen@gmail.com>
2016-11-27 22:17:37 -0800

    Style changes :/

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,27 +5,23 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className, parent, elements) {
-  if (elements == undefined) {
-    elements = [];
-  };
   if (parent == undefined) {
     parent = document.body;
   };
-
+  if (elements == undefined) {
+    elements = [];
+  };
   // termination condition
   if (arguments === 0) {
-    return "Please supply at least a class name."
+    return "Please supply a class name to search for."
   }
-
   // base case
   if (parent.classList.contains(className)) {
     elements.push(parent);
   }
-
   // recursive case
   for(var i = 0; i < parent.children.length; i++) {
     elements.concat(getElementsByClassName(className, parent.children[i], elements));
   }
-
   return elements;
 };
