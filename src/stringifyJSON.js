4002b12cc6b2b2eb93c54a88ed2fcb5a20190dca
Kay Christensen <kaychristensen@gmail.com>
2017-07-04 16:23:45 -0700

    structure and ES6 refactor

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,39 +3,56 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+const stringifyJSON = (obj) => {
 
-  // handles types boolean, null, and number
-  if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
-    return obj += '';
-
-  // handles type string
-  } else if (typeof obj === 'string') {
-    return '"' + obj + '"';
-
-  // handles arrays
-  } else if (Array.isArray(obj)) {
-    var result = '';
-    
-    _.each(obj, function(item) {
-      item = stringifyJSON(item);
-      result += item + ',';
-    });
-
-    return '[' + result.slice(0, result.length - 1) + ']';
-
-  // handles empty and unstringifiable objects
-  } else if (typeof obj === 'object' && (Object.keys(obj).length === 0 || Object.keys(obj).includes('functions'))) {
-    return '{}';
-
-  // handles stringifiable objects
-  } else if (typeof obj === 'object') {
-    var result = '';
-
-    _.each(obj, function(value, key) {
-      result += stringifyJSON(key) + ':' + stringifyJSON(value) + ',';
-    });
-
-    return '{' + result.slice(0, result.length - 1) + '}';
+  switch(checkType(obj)) {
+    case 'boolean':
+    case 'number':
+    case 'null':
+      return obj += '';
+      break;
+    case 'string':
+      return `"${obj}"`;
+      break;
+    case 'unstring':
+      return '{}';
+      break;
+    case 'array':
+      return `[${obj.map((item) => {
+        return stringifyJSON(item);
+      }).join(',')}]`;
+      break;
+    case 'object':
+      let results = [];
+      _.each(obj, function(value, key) {
+        results.push(`${stringifyJSON(key)}:${stringifyJSON(value)}`);
+      });
+      return `{${results.join(',')}}`
   }
 };
+
+
+const checkType = (obj) => {
+  let type = typeof obj;
+
+  if (typeof obj === 'boolean') {
+    type = 'boolean';
+  } else if (typeof obj === 'number') {
+    type = 'number';
+  } else if (obj === null) { 
+    type = 'null';
+  } else if (typeof obj === 'string') { 
+    type = 'string'; 
+  } else if (Array.isArray(obj)) { 
+    type = 'array';
+  } else {
+    type = 'object';
+    let keys = Object.keys(obj);
+    if (!keys.length || keys.includes('functions')) {
+      type = 'unstring';
+    }
+  }
+
+  return type;
+};
+
